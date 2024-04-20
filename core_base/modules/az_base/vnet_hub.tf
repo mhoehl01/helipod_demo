b55a97e0e6f4f8c3f9315e5a74bc21391bf1e72b
Höhl <MHOEHL@controlware.de>
2024-04-20 16:23:03 +0200

    fixed default subnet  prefixfor az hub vnet

--- core_base/modules/az_base/vnet_hub.tf
+++ core_base/modules/az_base/vnet_hub.tf
@@ -3,4 +3,10 @@ resource "azurerm_virtual_network" "core_hub" {
   location            = azurerm_resource_group.core_rg.location
   resource_group_name = azurerm_resource_group.core_rg.name
   address_space       = ["${var.address_space}"]
+
+  subnet {
+    name           = "default"
+    address_prefix = var.address_space
+  }
+
 }
