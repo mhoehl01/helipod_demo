f06bd17baee65d1eaa163e682d20dbd4e86ffb71
Höhl <MHOEHL@controlware.de>
2024-04-26 12:02:03 +0200

    changed code structure for az hub default subnet

--- core_base/modules/az_base/vnet_hub.tf
+++ core_base/modules/az_base/vnet_hub.tf
@@ -3,9 +3,11 @@ resource "azurerm_virtual_network" "core_hub" {
   location            = azurerm_resource_group.core_rg.location
   resource_group_name = azurerm_resource_group.core_rg.name
   address_space       = ["${var.hub_address_space}"]
+}
 
-  subnet {
-    name           = "Default"
-    address_prefix = var.defaultsubnet_address_prefix
-  }
+resource "azurerm_subnet" "core_default_subnet" {
+  name                 = "DefaultSubnet"
+  resource_group_name  = azurerm_resource_group.core_rg.name
+  virtual_network_name = azurerm_virtual_network.core_hub.name
+  address_prefixes     = ["${var.defaultsubnet_address_prefix}"]
 }
