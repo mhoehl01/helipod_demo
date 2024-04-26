d82442be475610ff0e11ebc0d1c903dbbcd21b3a
Höhl <MHOEHL@controlware.de>
2024-04-26 11:19:27 +0200

    format code

--- core_base/main.tf
+++ core_base/main.tf
@@ -6,18 +6,18 @@ module "az_base" {
 }
 
 module "aws_base" {
-  source              = "./modules/aws_base"
-  hub_cidr            = "10.20.0.0/16"
-  defaultsubnet_cidr  = "10.20.0.160/28"
-  gatewaysubnet_cidr  = "10.20.0.0/26"
+  source             = "./modules/aws_base"
+  hub_cidr           = "10.20.0.0/16"
+  defaultsubnet_cidr = "10.20.0.160/28"
+  gatewaysubnet_cidr = "10.20.0.0/26"
 }
 
 module "hyperscaler_peering" {
-  source = "./modules/hyperscaler_peering"
-  az_pubip            = module.az_base.core_gw_pubip
-  aws_gateway_id = module.aws_base.gateway_id
+  source                 = "./modules/hyperscaler_peering"
+  az_pubip               = module.az_base.core_gw_pubip
+  aws_gateway_id         = module.aws_base.gateway_id
   az_resource_group_name = module.az_base.resource_group_name
-  aws_hub_cidr = module.aws_base.hub_cidr
+  aws_hub_cidr           = module.aws_base.hub_cidr
 }
 
 
--- core_base/modules/aws_base/output.tf
+++ core_base/modules/aws_base/output.tf
@@ -1,9 +1,9 @@
-output gateway_id {
+output "gateway_id" {
   value       = aws_vpn_gateway.core_gw.id
   description = "Id of the aws vpn gateway"
 }
 
-output hub_cidr {
+output "hub_cidr" {
   value       = aws_vpc.core_hub.cidr_block
   description = "Cidr of the hub vpc"
 }
