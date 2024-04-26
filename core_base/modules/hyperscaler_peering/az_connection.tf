1b9ab9f8f99eb196bc63bb7de7c3db96b21b0b87
Höhl <MHOEHL@controlware.de>
2024-04-26 13:31:06 +0200

    use preshared key of aws vpn configuration

--- core_base/modules/hyperscaler_peering/az_connection.tf
+++ core_base/modules/hyperscaler_peering/az_connection.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network_gateway_connection" "az_connection" {
   virtual_network_gateway_id = var.az_gateway_id
   local_network_gateway_id   = azurerm_local_network_gateway.core_lgw.id
 
-  shared_key = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
+  shared_key = aws_vpn_connection.aws_connection.tunnel1_preshared_key
 }
 
 
